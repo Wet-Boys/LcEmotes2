4e5451c0076749b433dcb2fad35934a5a8d76779
Rune580 <runemaster580@gmail.com>
2024-01-07 10:24:10 -0500

    initial commit

---
+++ LcEmotes2AndKnucklesFeaturingDante/LcEmotes2AndKnucklesFeaturingDantePlugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+
+namespace LcEmotes2AndKnucklesFeaturingDante;
+
+[BepInPlugin(ModGuid, ModName, ModVersion)]
+public class LcEmotes2AndKnucklesFeaturingDantePlugin : BaseUnityPlugin
+{
+    public const string ModGuid = "com.gemumoddo.lc_emotes2_and_knuckles_featuring_dante";
+    public const string ModName = "Emotes 2 And Knuckles Featuring Dante";
+    public const string ModVersion = "1.0.0";
+
+    private void Awake()
+    {
+        
+    }
+}
