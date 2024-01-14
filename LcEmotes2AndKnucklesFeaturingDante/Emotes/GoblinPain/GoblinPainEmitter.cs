a6688055eaac16649f961fb3f82ba7681f327a4f
Rune580 <runemaster580@gmail.com>
2024-01-13 21:29:46 -0500

    goblin kind of

---
+++ LcEmotes2AndKnucklesFeaturingDante/Emotes/GoblinPain/GoblinPainEmitter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace LcEmotes2AndKnucklesFeaturingDante.Emotes.GoblinPain;
+
+public class GoblinPainEmitter : MonoBehaviour
+{
+    public ParticleSystem[] goblinParticleEmitter = [];
+    public float timeBetweenPulse = 0.25f;
+
+    private int _pulseIndex;
+    private float _timer;
+
+    private void Update()
+    {
+        _timer -= Time.unscaledDeltaTime;
+        
+        if (_timer > 0)
+            return;
+        
+        _timer = timeBetweenPulse;
+        goblinParticleEmitter[_pulseIndex].Play();
+
+        _pulseIndex++;
+        if (_pulseIndex >= goblinParticleEmitter.Length)
+            _pulseIndex = 0;
+    }
+}
